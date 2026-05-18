27bfbb7f478226ddc11f69d6671a9baafed36ad7
Juawo <joaopedrojp2703@gmail.com>
2026-05-18 12:28:04 -0300

    Refactor API communication to include headers for requests and improve error logging

--- src/api/communication_api.js
+++ src/api/communication_api.js
@@ -2,12 +2,18 @@ import { default as axios } from "axios";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
+const API_KEY = process.env.API_KEY
+const headers = {
+    "Content-Type": "application/json",
+    "X-API-KEY" : API_KEY
+}
 
 // Se comunica com a API, recebe as cartas que já existem via GET e envia as cartas geradas com POST - 1
 // Precisa refatorar!
 export async function getCardsFromCategory(category) {
     try {
-        let res = await axios.get(`${process.env.URL_API}api/card/Category/${category}`);
+        let res = await axios.get(`${process.env.URL_API}api/card/Category/${category}`, { headers });
+        console.log(`Status : ${res.status}`)
         return res.data;
     } catch (error) {
         console.error('Erro ao buscar cartas da categoria:', category, error.message);
@@ -16,10 +22,9 @@ export async function getCardsFromCategory(category) {
 }
 export async function createCard(createCardDto) {
     try {
-        await axios.post(`${process.env.URL_API}api/Card/`, createCardDto);
+        let res = await axios.post(`${process.env.URL_API}api/Card/`, createCardDto, { headers });
+        console.log(`Status : ${res.status}`)
     } catch (error) {
         console.error('Erro ao criar carta:', error.message);
     }
 }
-
-// getCardsFromCategory("pop")
